f439e76ddcb7bfbfed219a4e9a269153f0bd9149
Ivan HG <herreraleon14@gmail.com>
2024-09-14 12:20:53 -0600

    Pruebas

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const app = express();
 // Definir el puerto
 const port = 3000;
 
-app.use(cors({
-  origin: 'https://apex.oracle.com', // Especifica el dominio permitido o usa '*' para permitir todos.
-}));
-
 // Ruta GET en /prueba que devuelve un JSON
 app.get('/prueba', (req, res) => {
   res.json({
