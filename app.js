a69b9727394d87d8cf668f23052c030ca9e14ee0
Ivan HG <herreraleon14@gmail.com>
2024-09-14 12:18:05 -0600

    Permitir CORS

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 // Definir el puerto
 const port = 3000;
 
+app.use(cors({
+  origin: 'https://apex.oracle.com', // Especifica el dominio permitido o usa '*' para permitir todos.
+}));
+
 // Ruta GET en /prueba que devuelve un JSON
 app.get('/prueba', (req, res) => {
   res.json({
