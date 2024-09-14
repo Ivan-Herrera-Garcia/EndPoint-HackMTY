b2f4940242c7656d8b2f8b435ea7c130acbb9a1b
Ivan HG <herreraleon14@gmail.com>
2024-09-14 12:39:57 -0600

    Oracle apex

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = express();
 // Definir el puerto
 const port = 3000;
 
+const cors = require('cors'); // Importa la librería cors
+
+app.use(cors({
+  origin: 'https://apex.oracle.com', // Cambia esto a '*' si quieres permitir todos los orígenes
+}));
+
 // Ruta GET en /prueba que devuelve un JSON
 app.get('/prueba', (req, res) => {
   res.json({
